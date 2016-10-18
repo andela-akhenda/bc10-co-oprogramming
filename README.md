196d54a38577e1dee9095f01de163a6dd9586ef5
Joseph Akhenda <akhenda@gmail.com>
2016-10-18 15:27:02 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BC 10 Pair Programming Project - Co-oprogramming
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, url_for, redirect, make_response
+import pyrebase
+
+app = Flask(__name__)
+
+config = {
+  "apiKey": "AIzaSyDrCeJ-2yN_iB8znyDeG8ZRS39BiWdlcxE",
+  "authDomain": "co-oprogramming.firebaseapp.com",
+  "databaseURL": "https://co-oprogramming.firebaseio.com",
+  "storageBucket": "co-oprogramming.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+email="akhenda@gmail.com"
+password="123456789"
+
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password(email, password)
+
+
+@app.route('/',  methods=['GET', 'POST'])
+def index():
+    return "<h1>Hello, World!</h1>"    
+
+
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=5555, debug=True)
